cc70b6e231ab54be4d839085c700f21830ab0f1b
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-06-24 21:52:18 +0200

    Update genpass.py

--- genpass.py
+++ genpass.py
@@ -18,23 +18,23 @@ class GenerateKey:
 
     def __init__(
             self,
-            string_size  : int,
-            string_upper : str,
-            string_lower : str,
+            string_size: int,
+            string_upper: str,
+            string_lower: str,
             string_digits: str
     ):
         self.string_digits = string_digits
-        self.string_lower  = string_lower
-        self.string_upper  = string_upper
-        self.string_size   = string_size
+        self.string_lower = string_lower
+        self.string_upper = string_upper
+        self.string_size = string_size
 
     def gen_rand_str(self):
-        upper   = self.string_upper
-        lower   = self.string_lower
-        digit   = self.string_digits
-        rands   = "!§$%&/()=?{}][-_,;.:"
-        mixgs   = upper + lower + digit + rands
-        ssize   = self.string_size
+        upper = self.string_upper
+        lower = self.string_lower
+        digit = self.string_digits
+        rands = "!§$%&/()=?{}][-_,;.:"
+        mixgs = upper + lower + digit + rands
+        ssize = self.string_size
         shuffle = random.sample(mixgs, ssize)
         
         print(f"Your password: {''.join(shuffle)}")
